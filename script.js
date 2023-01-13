78527d723aeda1117087d304b11974c6b4125448
JeffersonLeiteF <jefferson.leite47@outlook.com>
2023-01-13 17:50:41 -0300

    Adição do JS
    Transição de Banner automática com JS, trocando os Radios a cada 10000ms e adição de Ease-in-out nos slides.

---
+++ script.js
@@ -0,0 +1,14 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 10000)
+
+function nextImage(){
+    count++;
+    if(count>3){
+        count = 1;
+    }    
+document.getElementById("radio"+count).checked = true;
+}
